3d8f6f035546d4347d5dc70b8e73c67e8234dc71
mindsgn <seni.tembe@protonmail.com>
2021-06-28 21:33:58 +0200

    card controller

---
+++ src/controller/cards.controller.ts
@@ -0,0 +1,14 @@
+import { RequestHandler } from "express";
+import { connect } from '../database';
+
+export const getCards: RequestHandler = async (req, res) => {
+  try {
+    const connection = await connect();
+    const posts = await connection.query(`select * from cards`);
+    await connection.end;
+    return res.status(200).json({success: true, data: posts[0], message: "message successfully posted"});
+  } catch (error) {
+    console.log(error);
+    return res.status(303).json({success: false, message: "message failed"});
+  }
+};
